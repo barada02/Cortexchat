85fb628f18a40da256a06420c1160748a6189ea3
barada02 <chandanbarada2@gmail.com>
2025-01-20 21:46:34 +0530

    text chunker function

--- setup.py
+++ setup.py
@@ -43,34 +43,32 @@ def setup_environment(session):
 
 def create_text_chunker_function(session):
     try:
-        session.sql("""
-            CREATE OR REPLACE FUNCTION text_chunker(pdf_text STRING)
-            RETURNS TABLE (chunk VARCHAR)
-            LANGUAGE PYTHON
-            RUNTIME_VERSION = '3.9'
-            HANDLER = 'text_chunker'
-            PACKAGES = ('snowflake-snowpark-python', 'langchain')
-            AS
-            $$
-            from snowflake.snowpark.types import StringType, StructField, StructType
-            from langchain.text_splitter import RecursiveCharacterTextSplitter
-            import pandas as pd
+        session.sql("""create or replace function text_chunker(pdf_text string)
+        returns table (chunk varchar)
+        language python
+        runtime_version = '3.9'
+        handler = 'text_chunker'
+        packages = ('snowflake-snowpark-python', 'langchain')
+        as
+        $$
+        from snowflake.snowpark.types import StringType, StructField, StructType
+        from langchain.text_splitter import RecursiveCharacterTextSplitter
+        import pandas as pd
 
-            class text_chunker:
+        class text_chunker:
+            
+            def process(self, pdf_text: str):
+                
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size = 1512,#Adjust this as you see fit
+                    chunk_overlap  = 256,#This let's text have some form of overlap. Useful for keeping chunks contextual
+                    length_function = len
+        )
 
-                def process(self, pdf_text: str):
-
-                    text_splitter = RecursiveCharacterTextSplitter(
-                        chunk_size = 1512,
-                        chunk_overlap  = 256,
-                        length_function = len
-                    )
-
-                    chunks = text_splitter.split_text(pdf_text)
-                    df = pd.DataFrame(chunks, columns=['chunks'])
-
-                    yield from df.itertuples(index=False, name=None)
-            $$
+        chunks = text_splitter.split_text(pdf_text)
+        df = pd.DataFrame(chunks, columns=['chunks'])
+        yield from df.itertuples(index=False, name=None)
+        $$;
         """).collect()
     except Exception as e:
         st.error(f"An error occurred while creating text_chunker function: {e}")
